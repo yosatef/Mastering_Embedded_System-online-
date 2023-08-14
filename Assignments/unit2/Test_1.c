2126a1b6472146148cdbaf5f3512da1c4cb5bdaa
Youstina Atef <youstinaatif43@gmail.com>
2023-08-14 20:29:03 +0200

    test code

---
+++ Assignments/unit2/Test_1.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Test_1.c
+ Author      : youstina
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+
